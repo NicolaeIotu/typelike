e1a5ebeb6b33b4ddfc0d4a1e31f74285d884631f
Iotu Nicolae <nicolae.g.iotu@gmail.com>
2020-11-22 21:59:18 +0200

    Add package-lock.json and Correct error cases
    Correct some errors with the handling of basic types
    
    Add tests to support basic types
    
    Add package-lock.json
    
    Signed-off-by: Iotu Nicolae <nicolae.g.iotu@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
-## v0.1.0 Initial release
+## v0.1.3 Add package-lock.json and Correct error cases
+Correct some errors with the handling of basic types
+Add tests to support basic types
+Add package-lock.json
 
+
+
+## v0.1.0 Initial release
--- test/typelike_basics.js
+++ test/typelike_basics.js
@@ -8,3 +8,15 @@ tap.throws(() => {
 tap.throws(() => {
   typelikeCustom({}, [])
 })
+
+tap.notOk(typelike({}, []))
+tap.notOk(typelike({}, { a: 4 }))
+tap.notOk(typelike({ a: 4 }, {}))
+tap.ok(typelike({}, {}))
+
+tap.ok(typelike(new Map(), new Map()))
+tap.notOk(typelike(new Map(), new Set()))
+tap.ok(typelike(new Set(), new Set()))
+
+tap.ok(typelike(1, 2323))
+tap.ok(typelike(true, false))
